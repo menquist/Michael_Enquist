82156b12f1aaaa19d87ed6a421e778b9f8c9e3c5
menquist <michael_enquist@hotmail.com>
2019-09-18 11:12:53 -0400

    Remove duplicated directory

--- R/AQM/Weekly Materials/Week 2/Week 2 activity Bank.R
+++
@@ -1,8 +0,0 @@
-Bank <- read.table("Bank.txt", header = T)
-dim(Bank)
-str(Bank)
-
-model <- lm(SALBEG ~ SEX + JOBCAT, data = Bank)
-summary(model)
-anova(model)
-#We are assuming that sex and job categories have an effect on
